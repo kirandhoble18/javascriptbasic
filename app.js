5f159ed53d506e08ed2d124d96730f7a40b848b2
kirandhoble18 <kirandhoble511@gmail.com>
2021-09-03 17:57:49 -0700

    console basics

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('hey this is my first piece of js code!!yehhh')
+console.log(12345698)
+console.log(true)
+console.log(false)
+
+
+console.warn("this is warning")
+
+console.error('this is error')
