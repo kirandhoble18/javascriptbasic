3cb9a148009b1734e7f1d869c8e55c7df1d585e4
kirandhoble18 <kirandhoble511@gmail.com>
2021-09-03 18:55:28 -0700

    array basics

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-console.log('hey this is my first piece of js code!!yehhh')
-console.log(12345698)
-console.log(true)
-console.log(false)
+let age=195866968745845;
+let name='asdrttg563546552879+6251146'
 
-
-console.warn("this is warning")
-
-console.error('this is error')
+let arr=[1,2,3,4,5]
+arr.push("value")
+arr.push("false")
+console.log(arr)
+console.log(typeof arr)
