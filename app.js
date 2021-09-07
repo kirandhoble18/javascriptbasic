202fc4ae4d3e91da300f3815a4ef3076d33f60df
kirandhoble18 <kirandhoble511@gmail.com>
2021-09-07 16:17:58 -0700

    comparison  opertors

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const a=2
-const b=15
-let c=0
+//comparison operators
+ const a=21
+ const b=7
+ const c=21
 
-console.log(a+b)  
-console.log(a-b)  
-console.log(a*b)  
-console.log(a/b)  
-console.log(a**b)  
-console.log(a%b)  
+ console.log('a>b',a>b)
+ console.log('a<b',a<b)
+ console.log('a==b',a==b)
+ console.log('a==c',a==c)
+console.log('a>=b',a>=b)
+
+console.log('---------------------------------------------------------------')
+//----------------------------------------------------
+
+const e=11
+const d='11'
+console.log(e==d)
+console.log(e===d)
+//e==d && number ==string
+console.log(true && false)
