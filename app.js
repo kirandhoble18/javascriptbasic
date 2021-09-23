cb022a6da679af23e1836b4f6c5ec767644457ef
kirandhoble18 <kirandhoble511@gmail.com>
2021-09-23 09:27:28 -0700

    function keyword

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const element = document.getElementById('selectme')
-console.log(element)
-element
+// const rock =document.querySelector('#rock')
+// const paper =document.querySelector('#paper')
+//const scissor =document.querySelector('#scissor')
+
+function logName(Name="kiran dhoble",age=18){
+  //console.log("kiran")
+   console.log(Name+  "  You are "+age)
+}
+
+logName()
+
+function add(a=0,b=0){
+return a+b
+}
+const result=add(12,99)
+console.log(result)
