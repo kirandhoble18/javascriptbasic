986220ac6ef254806ee5db4a2504a3476098306b
kirandhoble18 <kirandhoble511@gmail.com>
2021-09-22 18:01:43 -0700

    opertors

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-//comparison operators
- const a=21
- const b=7
- const c=21
-
- console.log('a>b',a>b)
- console.log('a<b',a<b)
- console.log('a==b',a==b)
- console.log('a==c',a==c)
-console.log('a>=b',a>=b)
-
-console.log('---------------------------------------------------------------')
-//----------------------------------------------------
-
-const e=11
-const d='11'
-console.log(e==d)
-console.log(e===d)
-//e==d && number ==string
-console.log(true && false)
+const element = document.getElementById('selectme')
+console.log(element)
+element
